825db885e89b29c2ec7ee27d35b0d2a136aedda7
whl9422 <whl9422@163.com>
2019-10-13 21:32:59 +0800

    增加question8 二叉树中的下一个节点

---
+++ offer/src/com/whl/offer/question8/TreeNode.java
@@ -0,0 +1,12 @@
+package com.whl.offer.question8;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode parent;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
