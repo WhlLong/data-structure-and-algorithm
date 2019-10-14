50a88135674a7c97b557c669af22f9b8b0cdfd06
wanghuailong <huailong.wang@ifchange.com>
2019-10-14 20:35:50 +0800

    更新剑指offer 18

--- offer/src/com/whl/offer/question18/DeleteDuplicationNode.java
+++ offer/src/com/whl/offer/question18/DeleteDuplicationNode.java
@@ -11,14 +11,15 @@ package com.whl.offer.question18;
  * <p>
  * <p>
  * <p>
- *  情况一: 出现重复节点，保留一个
- *  情况二: 出现重复节点，一个不留，全部删除
- *
+ * 情况一: 出现重复节点，保留一个
+ * 情况二: 出现重复节点，一个不留，全部删除
  */
 public class DeleteDuplicationNode {
 
     /**
      * 情况一： 出现重复节点时，保留其中一个重复节点
+     * 思路； 从头结点开始，如果发现当前节点和下一个节点重复，就删除下一个节点。
+     *
      * @param pHead
      * @return
      */
@@ -28,33 +29,63 @@ public class DeleteDuplicationNode {
             return null;
         }
 
-        /**
-         * 1.如果链表中只有一个节点，那么该链表中没有重复节点
-         */
-        if (pHead.next == null) {
-            return pHead;
+        ListNode head = pHead;
+        boolean isDup = false;
+        while (pHead.next != null) {
+            if (pHead.val == pHead.next.val) {
+                pHead.next = pHead.next.next;
+                isDup = true;
+            } else {
+                isDup = false;
+            }
+
+            if (!isDup) {
+                pHead = pHead.next;
+            }
         }
-        /**
-         * 2.如果该链表中有多个节点。
-         * 将链表的节点与它的后一个节点比较，如果相同就删除当前节点
-         */
 
-
-        return null;
+        return head;
     }
 
-
+    /**
+     * 情况二: 出现重复节点，一个不留，删除全部重复节点
+     *
+     * @param pHead
+     * @return
+     */
+    public ListNode deleteDuplication2(ListNode pHead) {
+        if (pHead == null) {
+            return null;
+        }
+        ListNode head = new ListNode(Integer.MIN_VALUE);
+        head.next = pHead;
+        ListNode pre = head;
+        boolean isDup = false;
+        while (pHead != null) {
+            if (pHead.next != null && pHead.val == pHead.next.val) {
+                while (pHead.next != null && pHead.val == pHead.next.val) {
+                    pHead = pHead.next;
+                }
+                pre.next = pHead.next;
+                pHead = pHead.next;
+            } else {
+                pre = pre.next;
+                pHead = pHead.next;
+            }
+        }
+        return head.next;
+    }
 
     public static void main(String[] args) {
         DeleteDuplicationNode deleteDuplicationNode = new DeleteDuplicationNode();
 
         ListNode listNode0 = new ListNode(1);
-        ListNode listNode1 = new ListNode(1);
-        ListNode listNode2 = new ListNode(1);
-        ListNode listNode3 = new ListNode(1);
-        ListNode listNode4 = new ListNode(1);
-        ListNode listNode5 = new ListNode(1);
-        ListNode listNode6 = new ListNode(2);
+        ListNode listNode1 = new ListNode(2);
+        ListNode listNode2 = new ListNode(3);
+        ListNode listNode3 = new ListNode(3);
+        ListNode listNode4 = new ListNode(5);
+        ListNode listNode5 = new ListNode(5);
+        ListNode listNode6 = new ListNode(7);
 
         listNode5.next = listNode6;
         listNode4.next = listNode5;
@@ -64,8 +95,8 @@ public class DeleteDuplicationNode {
         listNode0.next = listNode1;
 
 
-        deleteDuplicationNode.deleteDuplication1(listNode0);
-
+        ListNode head = deleteDuplicationNode.deleteDuplication2(listNode0);
+        System.out.println(head);
 
     }
 
--- offer/src/com/whl/offer/question8/BinaryTreeNextNode.java
+++ offer/src/com/whl/offer/question8/BinaryTreeNextNode.java
@@ -5,6 +5,9 @@ package com.whl.offer.question8;
  * 题目：
  * 给定一棵二叉树和其中一个节点，如何找出中序遍历序列的下一个节点？
  * 树中的节点除了有两个分别指向左右子节点的指针，还有一个指向父节点的指针。
+ *
+ *
+ *
  */
 public class BinaryTreeNextNode {
 
@@ -12,59 +15,24 @@ public class BinaryTreeNextNode {
         
     }
 
-    public TreeNode solution(TreeNode node) {
-        if (node == null) {
-            return null;
-        }
 
-        //1.如果当前节点没有父节点
-        if (node.parent == null) {
-            //1.1.如果该节点没有右子节点，那么就不存在下一个子节点。
-            if (node.right == null) {
-                return null;
-            } else {
-                //1.2.如果该节点有右子节点，那么下一个节点就是它的右子树的中序遍历的第一个节点。
-                node = node.right;
-                while (node.left != null) {
-                    node = node.left;
-                }
-                return node;
-            }
-        } else {
-            // 2.1.如果该节点是父节点的左子节点，那么下一个节点就是父节点。
-            if (node == node.parent.left) {
-                return node.parent;
-            } else {
-                //  2.2.如果该节点是父节点的右子节点:
-                //2.2.1.如果它有右子节点，下一个节点就是它的右子树的中序遍历的第一个节点。
-                if (node.right != null) {
-                    node = node.right;
-                    while (node.left != null) {
-                        node = node.left;
-                    }
-                    return node;
-                } else {
-                    //2.2.2.如果它没有右子节点，下一个节点就要考虑父节点，以它的父节点为当前节点，然后从 2.2.3开始执行。
-                    node = node.parent;
-                    //2.2.3.如果当前节点为空，则不存在下一个节点。
-                    while (node != null) {
-                        // 2.3.4如果当前节点的父节点为空，则不存在下一个节点。
-                        if (node.parent == null) {
-                            return null;
-                        } else {
-                            //2.2.5.如果当前节点是父节点的右子节点，以当前节点的父节点作为当前节点，然后从 2.2.3开始执行。
-                            if (node == node.parent.right) {
-                                node = node.parent;
-                            } else {
-                                //2.2.6.如果当前节点是父节点的左子节点，那么下一个节点就是当前节点的父节点。
-                                return node.parent;
-                            }
 
-                        }
-                    }
-                    return null;
-                }
+    public TreeLinkNode GetNext(TreeLinkNode pNode)
+    {
+        TreeLinkNode node;
+        if(pNode.right!=null) {
+            node=pNode.right;
+            while(node.left!=null) {
+                node=node.left;
             }
+            return node;
         }
+        node=pNode;
+        while(node.next!=null) {
+            if(node.next.left==node)
+                return node.next;
+            node=node.next;
+        }
+        return null;
     }
 }
